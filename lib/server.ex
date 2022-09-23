eccf355a634630f57722008098e9ae10de4ae22f
Artem Rakov <artem@rakov.me>
2022-09-23 17:44:16 -0400

    simple ping-pong message

--- lib/server.ex
+++ lib/server.ex
@@ -5,10 +5,38 @@ defmodule Server do
     Supervisor.start_link([{Task, fn -> Server.listen() end}], strategy: :one_for_one)
   end
 
-   def listen() do
+  def listen() do
     IO.puts("Logs from your program will appear here!")
 
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
     {:ok, _client} = :gen_tcp.accept(socket)
+  end
+
+  def serve(client_socket) do
+    client_socket
+    |> read_request
+    |> handle
+    |> serialze
+    |> write_response(client_socket)
+  end
+
+  def read_request(client_socket) do
+    {:ok, request} = :gen_tcp.recv(client_socket, 0)
+
+    request
+  end
+
+  def handle(_request) do
+    "PONG"
+  end
+
+  def serialze(response) do
+    "+#{response}/r/n"
+  end
+
+  def write_response(response, client_socket) do
+    :ok = :gen_tcp.send(client_socket, response)
+
+    :gen_tcp.close(client_socket)
   end
 end
