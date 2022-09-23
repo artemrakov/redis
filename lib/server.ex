e53ad26421cb58e68e135f64ecd8ddf29a8197cc
Artem Rakov <artem@rakov.me>
2022-09-23 18:07:18 -0400

    some fixes

--- lib/server.ex
+++ lib/server.ex
@@ -9,18 +9,14 @@ defmodule Server do
     IO.puts("Logs from your program will appear here!")
 
     {:ok, socket} = :gen_tcp.listen(6379, [:binary, active: false, reuseaddr: true])
-    accept_loop(socket)
+    accept(socket)
   end
 
-  def accept_loop(listen_socket) do
+  def accept(listen_socket) do
     IO.puts "Waiting for client connection"
 
     {:ok, client} = :gen_tcp.accept(listen_socket)
     Task.start(fn -> serve(client) end)
-
-    IO.puts "Connection accepted"
-
-    accept_loop(listen_socket)
   end
 
   def serve(client_socket) do
@@ -47,7 +43,5 @@ defmodule Server do
 
   def write_response(response, client_socket) do
     :ok = :gen_tcp.send(client_socket, response)
-
-    :gen_tcp.close(client_socket)
   end
 end
