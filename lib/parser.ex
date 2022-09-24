85bec8a6f230752c57b5bdcba66db5fbfa69b250
Artem Rakov <artem@rakov.me>
2022-09-24 17:34:38 -0400

    refactors

--- lib/parser.ex
+++ lib/parser.ex
@@ -1,14 +1,14 @@
 defmodule Parser do
   def parse(data) do
-    String.split(data, "\r\n", trim: true)
-    |> extract_commands
+    [type | items] = String.split(data, "\r\n", trim: true)
+    extract_commands(String.first(type), String.last(type), items)
   end
 
-  def extract_commands(["*0" | []]) do
+  def extract_commands("*", 0, []) do
     {"", []}
   end
 
-  def extract_commands([_ | items]) do
+  def extract_commands("*", _, items) do
     [[_, command] | args_with_data] = Enum.chunk_every(items, 2)
     args = Enum.map(args_with_data, fn [_, arg] -> arg end)
 
